b6c36b014a90393249e7c793dbe69839e46cca89
Adrian Lewis <zen_global@hotmail.com>
2018-02-25 02:09:28 +0000

    Switched to support ASP.NET Core 2.0 (work in progress)

--- src/Glimpse.Agent.AspNet.Mvc/Razor/ScriptInjectorRazorHost.cs
+++ src/Glimpse.Agent.AspNet.Mvc/Razor/ScriptInjectorRazorHost.cs
@@ -1,42 +1,42 @@
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.AspNetCore.Mvc.Razor.Directives;
-using Microsoft.AspNetCore.Razor.Chunks;
-using Microsoft.AspNetCore.Razor.Compilation.TagHelpers;
+//using System.Collections.Generic;
+//using System.Linq;
+//using Microsoft.AspNetCore.Mvc.Razor;
+//using Microsoft.AspNetCore.Mvc.Razor.Directives;
+//using Microsoft.AspNetCore.Razor.Chunks;
+//using Microsoft.AspNetCore.Razor.Compilation.TagHelpers;
 
-namespace Glimpse.Agent.Razor
-{
-    public class ScriptInjectorRazorHost : MvcRazorHost
-    {
-        public ScriptInjectorRazorHost(IChunkTreeCache codeTreeCache, ITagHelperDescriptorResolver resolver) 
-            : base(codeTreeCache, resolver)
-        {
-        }
+//namespace Glimpse.Agent.Razor
+//{
+//    public class ScriptInjectorRazorHost : MvcRazorHost
+//    {
+//        public ScriptInjectorRazorHost(IChunkTreeCache codeTreeCache, ITagHelperDescriptorResolver resolver) 
+//            : base(codeTreeCache, resolver)
+//        {
+//        }
 
-        private IReadOnlyList<Chunk> _overriddenChunks;
+//        private IReadOnlyList<Chunk> _overriddenChunks;
 
-        public override IReadOnlyList<Chunk> DefaultInheritedChunks
-        {
-            get
-            {
-                if (_overriddenChunks != null)
-                    return _overriddenChunks;
+//        public override IReadOnlyList<Chunk> DefaultInheritedChunks
+//        {
+//            get
+//            {
+//                if (_overriddenChunks != null)
+//                    return _overriddenChunks;
 
-                var original = base.DefaultInheritedChunks.ToArray();
-                var newArray = new Chunk[original.Length + 1];
+//                var original = base.DefaultInheritedChunks.ToArray();
+//                var newArray = new Chunk[original.Length + 1];
 
-                original.CopyTo(newArray, 0);
+//                original.CopyTo(newArray, 0);
 
-                newArray[original.Length] = new AddTagHelperChunk
-                {
-                    LookupText = "*, Glimpse.Agent.AspNet.Mvc"
-                };
+//                newArray[original.Length] = new AddTagHelperChunk
+//                {
+//                    LookupText = "*, Glimpse.Agent.AspNet.Mvc"
+//                };
 
-                _overriddenChunks = newArray;
+//                _overriddenChunks = newArray;
 
-                return _overriddenChunks;
-            }
-        }
-    }
-}
+//                return _overriddenChunks;
+//            }
+//        }
+//    }
+//}
